7e22d6c9753375041fc2486dc13f4b8f4a2f425a
cleilson <cleilsonrock@gmail.com>
2021-03-28 20:05:39 -0300

    chat e cores do chat

--- client/src/components.js
+++ client/src/components.js
@@ -1,76 +1,91 @@
 // responsavel por construir o layout
-import blessed from 'blessed'
+import blessed from "blessed";
 
-export default class ComponentBuilder{
-  #screen
-  #layout
-  #input
+export default class ComponentBuilder {
+  #screen;
+  #layout;
+  #input;
+  #chat;
+  #status;
+  #activityLog;
   constructor() {}
 
-  #baseComponent(){
+  #baseComponent() {
     return {
-      border: 'line',
+      border: "line",
       mouse: true,
-      keys:true,
-      scrollboar:{
-        ch:'',
-        inverse: true
+      keys: true,
+      scrollboar: {
+        ch: "",
+        inverse: true,
       },
       //habilita colocar cores e tags nos textos
-      tags:true
-    }
+      tags: true,
+    };
   }
 
-  setScreen({ title }){
-      this.#screen = blessed.screen({
-        smartCSR: true,
-        title
-      })
+  setScreen({ title }) {
+    this.#screen = blessed.screen({
+      smartCSR: true,
+      title,
+    });
 
-      this.#screen.key(['escape', 'q', 'C-c'], () => process.exit(0))
-      return this
+    this.#screen.key(["escape", "q", "C-c"], () => process.exit(0));
+    return this;
   }
 
-  setLayoutComponent(){
+  setLayoutComponent() {
     this.#layout = blessed.layout({
       parent: this.#screen,
-      width:'100%',
-      height:'100%',
-    })
+      width: "100%",
+      height: "100%",
+    });
 
-    return this
+    return this;
   }
 
-  setInputComponent(onEnterPressed){
+  setInputComponent(onEnterPressed) {
     const input = blessed.textarea({
       parent: this.#screen,
       bottom: 0,
-      height: '10%',
+      height: "10%",
       inputOnFocus: true,
-      padding:{
+      padding: {
         top: 1,
-        left: 2
+        left: 2,
       },
-      style:{
-        fg:'#fcfffe',
-        bg:'#353535'
-      }
+      style: {
+        fg: "#fcfffe",
+        bg: "#353535",
+      },
+    });
+
+    input.key("enter", onEnterPressed);
+    this.#input = input;
+
+    return this;
+  }
+
+  setChatComponent() {
+    this.#chat = blessed.list({
+      ...this.#baseComponent(),
+      parent: this.#layout,
+      align: 'left',
+      width: "50%",
+      height: "100%",
+      items: ['{bold}Messenger{/}']
     })
 
-    input.key('enter', onEnterPressed)
-    this.#input = input
-
     return this
-
   }
 
-  build(){
+  build() {
     const components = {
       screen: this.#screen,
-      input: this.#input
-    }
+      input: this.#input,
+      chat: this.#chat
+    };
 
-    return components
+    return components;
   }
-
 }
--- client/src/terminalController.js
+++ client/src/terminalController.js
@@ -2,7 +2,23 @@ import ComponentBuilder from "./components.js";
 
 
 export default class TerminalController {
+  #userCollors = new Map()
+
   constructor() {}
+
+  #pickColor() {
+    return `#` + ((1 << 24) * Math.random() | 0).toString(16) + `-fg`;
+  }
+
+  #getUserCollors(userName) {
+    if (this.#userCollors.has(userName)) 
+      return this.#userCollors.get(userName)
+
+    const collor = this.#pickColor()
+    this.#userCollors.set(userName, collor)
+
+    return collor
+  }
 
   #onInputReceived(eventEmitter){
     return function (){
@@ -12,14 +28,40 @@ export default class TerminalController {
     }
   }
 
+  #onMessageReceived({ screen, chat}){
+    return msg => {
+      const { userName, message } = msg
+      const collor = this.#getUserCollors(userName)
+
+      chat.addItem(`{${collor}}{bold}${userName}{/}: ${message}`)
+
+      screen.render()
+    }
+  }
+
+  #registerEvents(eventEmitter, components){
+    eventEmitter.on('message:received', this.#onMessageReceived(components))
+  }
+
   async initializeTables(eventEmitter) {
     const components = new ComponentBuilder()
     .setScreen({ title:'HackerChat - Cleilson José'})
     .setLayoutComponent()
     .setInputComponent(this.#onInputReceived(eventEmitter))
+    .setChatComponent()
     .build()
+
+    this.#registerEvents(eventEmitter, components)
 
     components.input.focus()
     components.screen.render()
+
+    //teste
+    setInterval(() => {
+      eventEmitter.emit('message:received', { message: 'Oii', userName:'cleilson José'})
+      eventEmitter.emit('message:received', { message: 'Oii', userName:'juliana'})
+      eventEmitter.emit('message:received', { message: 'Olá', userName:'israel'})
+      eventEmitter.emit('message:received', { message: 'Opa', userName:'livia'})
+    },1000);
   }
 }
